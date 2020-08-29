9091d77a959ba8e2174a89e39830e9bebdf781d7
kouki.shinoda <shinosora-i0204@ezweb.ne.jp>
2020-08-29 15:42:48 +0000

    add column

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,8 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
   def index
-    @users = User.order(id: :desc).page(params[:page]).per(25)
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def new
@@ -28,5 +25,5 @@ class UsersController < ApplicationController
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+  end  
 end
