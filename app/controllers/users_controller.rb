9ebfb80a02e487ddd7365a1d7a1dfc8fa0e871a4
kouki.shinoda <shinosora-i0204@ezweb.ne.jp>
2020-09-01 13:14:04 +0000

    user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-    before_action :require_user_logged_in, only: [:index, :show]
+  before_action :logged_in_user, only:[:edit, :update, :destroy]
+  
   def index
   end
 
@@ -15,7 +16,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to tasks_url
+      redirect_to root_url
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
