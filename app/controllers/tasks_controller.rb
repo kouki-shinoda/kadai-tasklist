14568d6b89ae5b758264e9de823192ffdda520e5
kouki.shinoda <shinosora-i0204@ezweb.ne.jp>
2020-09-02 15:36:12 +0000

    login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,11 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-before_action :require_user_logged_in, only: [:create, :show, :destroy, :update]
+before_action :set_task, only: [:show, :edit, :update, :destroy]
+before_action :require_user_logged_in
+before_action :correct_user, only: [:show, :edit, :update, :destroy]
   def index
-    @tasks = Task.all
+    @task = current_user.tasks.build
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    #task.all
   end
 
   def show
@@ -13,12 +16,13 @@ before_action :require_user_logged_in, only: [:create, :show, :destroy, :update]
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = "Task が正常に作成されました"
-      redirect_to @task
+      redirect_to root_url
     else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = "Task が作成されませんでした"
       render :new
     end
@@ -52,5 +56,12 @@ before_action :require_user_logged_in, only: [:create, :show, :destroy, :update]
 
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
---
+++ db/migrate/20200902120947_adduser_totask.rb
@@ -0,0 +1,4 @@
+class AdduserTotask < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
