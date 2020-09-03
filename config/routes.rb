fa1e332a0a34bd50a14a270c3981c5ab38eadd4a
kouki.shinoda <shinosora-i0204@ezweb.ne.jp>
2020-09-03 12:13:48 +0000

    login

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
    #root to: "toppages#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :users, only: [:new, :create]
   resources :tasks
 end
